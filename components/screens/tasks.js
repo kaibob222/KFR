7fa8d07a9f28791e4a5d98d9ae499e09a97a77d1
kaibob222 <asas200097@gmail.com>
2024-04-04 17:12:13 +0900

    Add empty task screen

---
+++ components/screens/tasks.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import {Text, View, Button, StyleSheet, TouchableOpacity} from 'react-native';
+
+const Tasks = () => {
+
+  return (
+    <View >
+      
+    </View>
+  );
+};
+
+
+export default Tasks;
