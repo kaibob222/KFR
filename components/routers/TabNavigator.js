af38dc7b5f81dcf276b05e1218f802ba6011d493
Zeno3301 <113169886+Zeno3301@users.noreply.github.com>
2024-04-18 17:43:45 +0900

    Add notes and tasks icons in TabNavigator, instead of tabBarShowLabel and tabBarStyle used screenOptions

--- components/routers/TabNavigator.js
+++ components/routers/TabNavigator.js
@@ -1,6 +1,6 @@
-import React, {useEffect} from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Image, StyleSheet} from 'react-native';
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image, StyleSheet } from 'react-native';
 import TaskManager from '../screens/Tasks/tasks';
 import Notes from '../screens/notes';
 
@@ -8,13 +8,23 @@ const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          display: 'flex',
+        },
+      }}
+    >
       <Tab.Screen
         name="Tasks"
         component={TaskManager}
         options={{
-          tabBarIcon: ({focused}) => (
-            null
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? require('../../assets/icons/task_icon_active.png') : require('../../assets/icons/task_icon_inactive.png')}
+              style={styles.icon}
+            />
           ),
         }}
       />
@@ -22,20 +32,22 @@ const TabNavigator = () => {
         name="Notes"
         component={Notes}
         options={{
-          tabBarIcon: ({focused}) => (
-            null
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? require('../../assets/icons/notes_icon_active.png') : require('../../assets/icons/notes_icon_inactive.png')}
+              style={styles.icon}
+            />
           ),
         }}
       />
-      
-      </Tab.Navigator>
+    </Tab.Navigator>
   );
 };
 
 const styles = StyleSheet.create({
   icon: {
-    width: 35,
-    height: 35,
+    width: 60,
+    height: 30,
   },
 });
 
--- components/routers/authorization.js
+++ components/routers/authorization.js
@@ -29,7 +29,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
-    fontFamily: 'Inconsolata',
     fontWeight:'bold',
     fontSize: 15,
   },
